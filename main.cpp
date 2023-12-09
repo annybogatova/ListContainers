87756033b5954a4d0b2dfbb8e388592570806b2d
Anna <annybogatova@gmail.com>
2023-12-10 02:02:22 +0300

    Abstract List class

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <list>
+#include "ArrayList.h"
+#include "LinkedList.h"
+
+int main() {
+
+    LinkedList<int> *linkedList = new LinkedList<int>();
+
+    ArrayList<int> *arrayList = new ArrayList<int>(5);
+
+    return 0;
+}
