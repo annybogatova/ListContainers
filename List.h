bff80fe316b214b8dccd012885c3a55efcf16281
Anna <annybogatova@gmail.com>
2023-12-12 17:17:58 +0300

    add List: virtual methods with iterators

--- List.h
+++ List.h
@@ -5,14 +5,17 @@
 #ifndef LAB3_LIST_H
 #define LAB3_LIST_H
 
-#include <cstddef>
+#include "Iterator.h"
 
 template <typename T>
 class List {
 
+    using iterator = Iterator<T>;
     virtual void push_back(const T& data) = 0;
     virtual void insert(std::size_t position, const T &data) = 0;
+    virtual void insert(iterator position, const T &data) = 0;
     virtual void erase(std::size_t position) = 0;
+    virtual void erase(iterator position) = 0;
     virtual std::size_t size() = 0;
 
 };
